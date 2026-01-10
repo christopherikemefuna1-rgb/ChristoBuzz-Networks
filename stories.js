6a62e62a0354fcfa0f822dbcbf254c46267517e8
Christoking <christopherikemefuna1@gmail.com>
2026-01-10 17:30:32 +0100

    Create stories.js

---
+++ stories.js
@@ -0,0 +1,6 @@
+export function loadStories(container){
+  container.innerHTML=`<div class="stories">
+  <div class="story"><img src="https://via.placeholder.com/70"><small>User1</small></div>
+  <div class="story"><img src="https://via.placeholder.com/70"><small>User2</small></div>
+  </div>`;
+}
