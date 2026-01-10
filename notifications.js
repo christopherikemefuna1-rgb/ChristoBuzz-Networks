9014e5b20f609a671b5416b91b67bb09b5ef680c
Christoking <christopherikemefuna1@gmail.com>
2026-01-10 09:38:50 +0100

    Delete notifications.js

--- notifications.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from './supabase.js';
-
-export async function loadNotifications() {
-  const { data } = await supabase.from('notifications').select('*').order('created_at', { ascending: false });
-  data?.forEach(n => {
-    console.log('Notification:', n.message);
-    // Can be upgraded to show UI toast later
-  });
-}
