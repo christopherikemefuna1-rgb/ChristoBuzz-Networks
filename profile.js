52dbeadf38c4a763d1a2af983d928354aced5a18
Christoking <christopherikemefuna1@gmail.com>
2026-01-10 17:33:02 +0100

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,12 @@
+export function loadProfile(container){
+  container.innerHTML=`
+  <div class="profile-cover"><img src="https://via.placeholder.com/430x220"></div>
+  <div class="profile-header">
+  <img class="profile-avatar" src="https://via.placeholder.com/120">
+  <div class="profile-info">
+  <h2>Demo User</h2><p class="profile-bio">Demo bio</p>
+  </div></div>
+  <div class="profile-posts">
+  <img src="https://via.placeholder.com/140"><img src="https://via.placeholder.com/140">
+  </div>`;
+}
