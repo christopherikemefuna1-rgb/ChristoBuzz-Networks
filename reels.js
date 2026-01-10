10863044af708e42039287aeb8cfacc04a1366cf
Christoking <christopherikemefuna1@gmail.com>
2026-01-10 17:31:23 +0100

    Create reels.js

---
+++ reels.js
@@ -0,0 +1,3 @@
+export function loadReels(container){
+  container.innerHTML=`<div class="reel"><video src="https://www.w3schools.com/html/mov_bbb.mp4" controls autoplay muted loop></video></div>`;
+}
